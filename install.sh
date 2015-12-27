f54f6097c7943dabfffeb8404748e0b39f13bb05
Phil Huang <pichuang@cs.nctu.edu.tw>
2015-12-28 03:10:02 +0800

    Change install method

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function Install_Ansible() {
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible | grep "install ok installed")
+if [ "" == "$PKG_OK" ]; then
+    echo "No ansible. Install ansible."
+    sudo apt-get update
+    sudo apt-get install -y software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get install -y ansible
+    ansible --version
+fi
+
+}
+
+function Install_ONOS() {
+
+ansible-playbook -i hosts playbook.yml -k -K
+
+}
+
+Install_Ansible
+Install_ONOS
--- install_ansible.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-ansible --version
