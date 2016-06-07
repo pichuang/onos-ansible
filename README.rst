d5d1e288cd6ac2d1fecc3aac1b2790257a156b46
Phil Huang <pichuang@cs.nctu.edu.tw>
2016-06-07 13:30:38 +0800

    update
    Signed-off-by: Phil Huang <pichuang@cs.nctu.edu.tw>

--- README.rst
+++ README.rst
@@ -3,11 +3,16 @@ ONOS-Ansible Installer
 
 Installation of ONOS building enviroments
 
+Required
+
+- Ansible
+- If not install, please use `install_ansbile_from_ppa.sh`
+
 Step by Step
 
 1. Check variable in hosts and group_vars/config.yml
 
 2. Install by script
 
-   `./install.sh`
+   `ansible-playbook -i hosts playbook.yml -k -K --connection=local`
 
--- install_ansible_from_ppa.sh
+++ install_ansible_from_ppa.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-MASTER_HOSTNAME=$(hostname)
-
 function Install_Ansible() {
 
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible | grep "install ok installed")
@@ -17,20 +15,4 @@ fi
 
 }
 
-function Replace_Hostname() {
-
-if ! grep --quiet $MASTER_HOSTNAME hosts; then
-    sed -i "s/onos_build_machine/$MASTER_HOSTNAME/g" hosts
-fi
-
-}
-
-function Install_ONOS() {
-
-ansible-playbook -i hosts playbook.yml -k -K --connection=local
-
-}
-
 Install_Ansible
-Replace_Hostname
-Install_ONOS
